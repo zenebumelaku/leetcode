8684b6d30525ae88f4547ffbfaccbff3db0d41a7
zenebumelaku <melakuzenebu@gmail.com>
2025-12-02 17:26:03 +0300

    Time: 82 ms (86.81%) | Memory: 15.8 MB (38.87%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,19 +1,17 @@
 class Solution(object):
     def firstUniqChar(self, s):
-        # Create a dictionary to store the frequency of each character
+      
         char_freq = {}
 
-        # Iterate through the string to count character frequencies
+        
         for char in s:
             if char in char_freq:
                 char_freq[char] += 1
             else:
                 char_freq[char] = 1
 
-        # Iterate through the string again to find the first non-repeating character
         for i in range(len(s)):
             if char_freq[s[i]] == 1:
                 return i
 
-        # If no non-repeating character is found, return -1
         return -1
