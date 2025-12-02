f376f0cc3b07f68f112050d111b8b069b3e83af3
zenebumelaku <melakuzenebu@gmail.com>
2025-12-02 10:44:04 +0300

    Time: 0 ms (100.00%) | Memory: 12.3 MB (98.71%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def backspaceCompare(self, s, t):
+        stack_s, stack_t = [], []
+
+        for c in s:
+            if c != '#':
+                stack_s.append(c)
+            elif stack_s:
+                stack_s.pop()
+
+        for c in t:
+            if c != '#':
+                stack_t.append(c)
+            elif stack_t:
+                stack_t.pop()
+
+        return stack_s == stack_t
